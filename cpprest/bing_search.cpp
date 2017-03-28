5ca219fdebb62a240d4b17e12bf11440b0a7bf0d
吴凡 <wufan@wufandeMacBook-Pro-2.local>
2017-03-28 15:52:19 +0800

    bing search sample

--- cpprest/bing_search.cpp
+++ cpprest/bing_search.cpp
@@ -27,6 +27,21 @@ int main(int argc, char **argv) {
     const std::string outputFileName = argv[2];
 
     auto fileBuffer = std::make_shared<concurrency::streams::streambuf<uint8_t>>();
-    concurrency::streams::file_buffer<uint8_t>::open(outputFileName, std::ios::out).then(=)
+    concurrency::streams::file_buffer<uint8_t>::open(outputFileName, std::ios::out)
+            .then([=](concurrency::streams::streambuf<uint8_t> buffer) -> pplx::task<web::http::http_response> {
+                *fileBuffer = buffer;
+
+                web::http::client::http_client client("https://cn.bing.com", client_config_for_proxy());
+                return client.request(web::http::methods::GET,
+                                      web::http::uri_builder("/search").append_query("q", searchTerm).to_string());
+            })
+            .then([=](web::http::http_response response) -> pplx::task<size_t> {
+                printf("Response status: %d", response.status_code());
+                return response.body().read_to_end(*fileBuffer);
+            })
+            .then([=](size_t size) {
+                return fileBuffer->close();
+            })
+            .wait();
     return 0;
 }
